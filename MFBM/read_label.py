b6575657c2e640c9b8fea1362ae8b6cbb45a5ccb
unknown <879833043@qq.com>
2019-06-15 18:45:11 +0800

    read data

--- MFBM/read_label.py
+++ MFBM/read_label.py
@@ -7,38 +7,50 @@ def read():
     with open(file, mode="r", encoding="utf-8") as fp:
         lines = fp.readlines()
         chs, pos = list(), list()
-        inx = 0
+        comment = list()
         for line in lines:
             if 1 == len(line):
-                data.append([chs, pos])
+                inx = 0
+                size = len(comment)
+                while inx < size:
+                    hanz, label = comment[inx]
+                    if "B-T" == label:
+                        start = inx
+                        inx += 1
+                        while inx < size and "I-T" == comment[inx][1]:
+                            inx += 1
+                        pos.append([start, inx])
+                    elif "I-T" == label:
+                        pass
+                        print("没有出现B-T")
+                        inx += 1
+                    elif "0" == label:
+                        inx += 1
+                    else:
+                        print("未知情况")
+                        print(label)
+                        inx += 1
+
+                text = "".join(chs)
+                data.append([text, pos])
                 del chs
                 del pos
                 chs, pos = list(), list()
-                inx = 0
-                continue
-
-            # _label 样本打标
-            # label  模型打标
-            hanz, _label, label = line.split()
-            used_label = _label
-            chs.append(hanz)
-
-            if "B-T" == used_label:
-                # 找到结束
-                pass
-            elif "I-T" == used_label:
-                print("没有出现B-T")
-                print(label)
-            elif "0" == used_label:
-                pass
+                del comment
+                comment = list()
             else:
-                print("未知情况")
-                print(label)
-
-            inx += 1
+                # _label 样本打标
+                # label  模型打标
+                hanz, _label, label = line.split()
+                used_label = _label
+                comment.append([hanz, used_label])
+                chs.append(hanz)
 
     return data
 
 
 if __name__ == '__main__':
-    pass
+    data = read()
+    print("len data:", len(data))
+    # for item in data:
+    #     print(item)
